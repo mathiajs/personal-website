79c21b46b5ca7854698d9d0a7f97ca1725a4fcb1
Mathias Skogen-Karstensen <mathiajs@stud.ntnu.no>
2025-05-02 08:35:31 +0200

    Add SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Header from '@/components/header'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -24,9 +23,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='!scroll-smooth'>
-      <Head>
-        <meta name="google-site-verification" content="dScafSCpNG-XTBWATi4oJLfvSRHzLyaN1GlXubUax3M" />
-      </Head>
       <body className={`${inter.className} bg-slate-50 text-slate-950 relative pt-28 sm:pt-36 dark:bg-slate-800 dark:text-slate-50 dark:text-opacity-90`}>
         <div className="bg-[#e2fbf5] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>

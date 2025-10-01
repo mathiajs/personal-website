b4c402a1bdf0c3399d291a0676b3ea49a40cd278
Mathias Skogen-Karstensen <mathiajs@stud.ntnu.no>
2025-10-01 22:51:26 +0200

    feat: update information, experience and CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,129 +1,137 @@
 "use client";
 
-import Image from 'next/image';
-import React from 'react';
-import profilepicImg from '@/public/profilepic.jpg';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
-import { HiDownload } from 'react-icons/hi';
-import { FaGithubSquare } from 'react-icons/fa';
-import { useSectionInView } from '@/lib/hooks';
-import { useActiveSectionContext } from '@/context/active-section-context';
+import Image from "next/image";
+import React from "react";
+import profilepicImg from "@/public/profilepic.jpg";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
-  const { ref } = useSectionInView('Home', 0.5);
-  const {
-    setActiveSection,
-    setTimeOfLastClick,
-  } = useActiveSectionContext();
-
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
-    <section ref={ref} id='home' className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
-        <div className='flex items-center justify-center'>
-            <div className='relative'>
-              <motion.div
-                initial={{ scale: 0, opacity: 0}}
-                animate={{ scale: 1, opacity: 1}}
-                transition={{
-                  type: 'tween',
-                  delay: 0.4,
-                  duration: 0.25,
-                }}
-              >
-                <Image
-                  src={profilepicImg}
-                  alt='Profile picture'
-                  width={192}
-                  height={192}
-                  quality={95}
-                  priority={true}
-                  className='h-24 w-24 rounded-full border-[0.35rem] border-white shadow-xl'
-                />
-              </motion.div>
-
-
-              <motion.span className='absolute bottom-0 right-0 text-4xl'
-                initial={{ opacity: 0, scale: 0}}
-                animate={{ opacity: 1, scale: 1}}
-                transition={{
-                  type: 'spring',
-                  stiffness: 125,
-                  delay: 0.5,
-                  duration: 0.5,
-                }}
-              >
-                👋
-              </motion.span>
-            </div>
-        </div>
-
-        <motion.h1 className='mb-10 mt-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
-          initial={{ opacity: 0, y: 100}}
-          animate={{ opacity: 1, y: 0}}
-          transition={{
-            delay: 0.3,
-          }}
-        >
-          <span className='font-bold'>Hi, I'm Mathias</span>. I'm a{" "}
-          <span className='font-bold'>third year computer science student</span> at NTNU in Trondheim, Norway. I enjoy learning new technologies and building cool stuff. I'm currently studying{" "}
-          <span className='italic'>AI and machine learning,</span>  and I'm also interested in{" "} 
-          <span className='underline'>full-stack development.</span> 
-        </motion.h1>
-
-        <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
-          initial={{ opacity: 0, y: 100}}
-          animate={{ opacity: 1, y: 0}}
-          transition={{
-            delay: 0.4,
-          }}
-        >
-          {
-          //Contact button
-          }
-          <Link 
-            href='#contact'
-            className='group bg-slate-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-slate-950 active:scale-105 transition'
-            onClick={() => {
-              setActiveSection('Contact');
-              setTimeOfLastClick(Date.now());
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{
+              type: "tween",
+              delay: 0.4,
+              duration: 0.25,
             }}
           >
-            Contact me here 
-            <BsArrowRight
-            className='opacity-70 group-hover:translate-x-1 transition' />
-          </Link>
+            <Image
+              src={profilepicImg}
+              alt="Profile picture"
+              width={192}
+              height={192}
+              quality={95}
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
 
-          {
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.5,
+              duration: 0.5,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+        }}
+      >
+        <span className="font-bold">Hi, I'm Mathias</span>. I'm a{" "}
+        <span className="font-bold">fourth year computer science student</span>{" "}
+        at NTNU in Trondheim, Norway. I enjoy learning new technologies and
+        building cool stuff. I'm currently studying{" "}
+        <span className="italic">AI and machine learning,</span> and I'm also
+        interested in <span className="underline">full-stack development.</span>
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.4,
+        }}
+      >
+        {
+          //Contact button
+        }
+        <Link
+          href="#contact"
+          className="group bg-slate-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-slate-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        {
           //Download CV button
-          }
-          <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10' 
-            href='/CV.pdf' download={true}>
-            Download CV 
-            <HiDownload
-            className='opacity-60 group-hover:translate-y-1 transition' />
-          </a>
+        }
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/CV.pdf"
+          download={true}
+        >
+          Download CV
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
 
-          {
+        {
           //LinkedIn button
-          }
-          <a className='bg-white p-4 text-slate-700 hover:text-slate-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
-            href='https://www.linkedin.com/in/mathias-skogen-karstensen-0a8a1926a/' target='_blank'  
-          >
-            <BsLinkedin />
-          </a>
+        }
+        <a
+          className="bg-white p-4 text-slate-700 hover:text-slate-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/mathias-skogen-karstensen-0a8a1926a/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
 
-          {
+        {
           //Github button
-          }
-          <a className='bg-white p-4 text-slate-700 hover:text-slate-950 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
-            href='https://github.com/mathiajs' target='_blank'
-          >
-            <FaGithubSquare />
-          </a>
-        </motion.div>
+        }
+        <a
+          className="bg-white p-4 text-slate-700 hover:text-slate-950 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/mathiajs"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
-  )
+  );
 }
-
--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
+import { FaConnectdevelop, FaDesktop, FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import { GiTeacher } from "react-icons/gi";
 import { PiStudentBold } from "react-icons/pi";
@@ -38,25 +38,42 @@ export const experiencesData = [
   {
     title: "Research Assistant",
     location: "Trondheim, Norway",
-    description: "I'm working as a research assistant in the course tdt4100 at NTNU. My job involves holding lectures and being involved with the daily operation of the course",
+    description:
+      "I'm currently working as a research assistant in the course tdt4109 at NTNU. My job involves holding lectures and being involved with the daily operation of the course, I formerly worked the same job in course tdt4100.",
     icon: React.createElement(GiTeacher),
+    date: "January 2025-present",
+  },
+  {
+    title: "Software Developer Volunteer",
+    location: "Trondheim, Norway",
+    description:
+      "I'm currently working as a software developer volunteer for NTNUI Sprint. My job involves ensuring that all IT solutions of the organization are up to date, while also developing new features requested by the club.",
+    icon: React.createElement(GiTeacher),
+    date: "September 2025-present",
+  },
+  {
+    title: "Software Developer Intern",
+    location: "Lysaker, Norway",
+    description:
+      "I worked as a software developer intern at Genus, the leading no-code software development company in the Nordics. My project involved developing a support system to make handling secrets easier for developers using their platform.",
+    icon: React.createElement(FaDesktop),
     date: "2025-present",
   },
   {
     title: "Teaching Assistant",
     location: "Trondheim, Norway",
-    description: 
+    description:
       "I worked as a teaching assistant for the course TDT4109 at NTNU. I was responsible for designing and overseeing realistic mock exams for one of Norway’s largest courses, ensuring assessment accuracy and relevance.",
     icon: React.createElement(GiTeacher),
-    date: "2024-2024",
+    date: "August 2024-December 2024",
   },
   {
     title: "Learning Assistant",
     location: "Trondheim, Norway",
-    description: 
+    description:
       "I worked as a learning assistant for the courses TDT4109 and TDT4100 at NTNU. I helped students with their assignments and approved/disproved their assignments.",
     icon: React.createElement(PiStudentBold),
-    date: "2023-2024",
+    date: "August 2023-May 2024",
   },
   {
     title: "MSc in Computer Science",
@@ -64,19 +81,16 @@ export const experiencesData = [
     description:
       "I studied computer science at NTNU. I learned about algorithms, data structures, databases, web development, and more.",
     icon: React.createElement(LuGraduationCap),
-    date: "2022-present",
+    date: "August 2022-present",
   },
   {
     title: "Retail Worker",
     location: "Sarpsborg, Norway",
-    description: 
+    description:
       "I worked as a retail worker at a grocery store, where I learned about customer service, teamwork, and responsibility. I worked my way up to having the reponsibility of closing the store.",
     icon: React.createElement(CgWorkAlt),
-    date: "2018-present",
+    date: "2018-2024",
   },
-
-
-
 ] as const;
 
 export const projectsData = [

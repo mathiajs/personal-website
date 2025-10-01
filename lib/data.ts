7b3ee4ff9086a3379f245659ef893e7d318a5eb5
Mathias Skogen-Karstensen <mathiajs@stud.ntnu.no>
2025-10-01 22:54:49 +0200

    fix: derp

--- lib/data.ts
+++ lib/data.ts
@@ -41,7 +41,7 @@ export const experiencesData = [
     description:
       "I'm currently working as a research assistant in the course tdt4109 at NTNU. My job involves holding lectures and being involved with the daily operation of the course, I formerly worked the same job in course tdt4100.",
     icon: React.createElement(GiTeacher),
-    date: "January 2025-present",
+    date: "January 2025 - present",
   },
   {
     title: "Software Developer Volunteer",
@@ -49,7 +49,7 @@ export const experiencesData = [
     description:
       "I'm currently working as a software developer volunteer for NTNUI Sprint. My job involves ensuring that all IT solutions of the organization are up to date, while also developing new features requested by the club.",
     icon: React.createElement(GiTeacher),
-    date: "September 2025-present",
+    date: "September 2025 - present",
   },
   {
     title: "Software Developer Intern",
@@ -57,7 +57,7 @@ export const experiencesData = [
     description:
       "I worked as a software developer intern at Genus, the leading no-code software development company in the Nordics. My project involved developing a support system to make handling secrets easier for developers using their platform.",
     icon: React.createElement(FaDesktop),
-    date: "2025-present",
+    date: "June 2025 - August 2025",
   },
   {
     title: "Teaching Assistant",
@@ -65,7 +65,7 @@ export const experiencesData = [
     description:
       "I worked as a teaching assistant for the course TDT4109 at NTNU. I was responsible for designing and overseeing realistic mock exams for one of Norway’s largest courses, ensuring assessment accuracy and relevance.",
     icon: React.createElement(GiTeacher),
-    date: "August 2024-December 2024",
+    date: "August 2024 - December 2024",
   },
   {
     title: "Learning Assistant",
@@ -73,7 +73,7 @@ export const experiencesData = [
     description:
       "I worked as a learning assistant for the courses TDT4109 and TDT4100 at NTNU. I helped students with their assignments and approved/disproved their assignments.",
     icon: React.createElement(PiStudentBold),
-    date: "August 2023-May 2024",
+    date: "August 2023 - May 2024",
   },
   {
     title: "MSc in Computer Science",
@@ -81,7 +81,7 @@ export const experiencesData = [
     description:
       "I studied computer science at NTNU. I learned about algorithms, data structures, databases, web development, and more.",
     icon: React.createElement(LuGraduationCap),
-    date: "August 2022-present",
+    date: "August 2022 - present",
   },
   {
     title: "Retail Worker",
@@ -89,7 +89,7 @@ export const experiencesData = [
     description:
       "I worked as a retail worker at a grocery store, where I learned about customer service, teamwork, and responsibility. I worked my way up to having the reponsibility of closing the store.",
     icon: React.createElement(CgWorkAlt),
-    date: "2018-2024",
+    date: "2018 - 2024",
   },
 ] as const;
 
